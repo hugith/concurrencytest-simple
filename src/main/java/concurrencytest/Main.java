ea7cecd76d8b7ea43b0c89e6ac9aac7fe6d6816f
Hugi Thordarson <hugi@karlmenn.is>
2017-11-25 16:28:30 +0000

    Delete the "Person" class to simplify the project

--- src/main/java/concurrencytest/Main.java
+++ src/main/java/concurrencytest/Main.java
@@ -1,24 +1,18 @@
 package concurrencytest;
 
 import org.apache.cayenne.ObjectContext;
+import org.apache.cayenne.access.DataContext;
 import org.apache.cayenne.commitlog.CommitLogListener;
 import org.apache.cayenne.commitlog.CommitLogModule;
 import org.apache.cayenne.commitlog.model.ChangeMap;
 import org.apache.cayenne.configuration.server.ServerRuntime;
 import org.apache.cayenne.configuration.server.ServerRuntimeBuilder;
-import org.apache.cayenne.query.ObjectSelect;
+import org.apache.cayenne.query.SelectQuery;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-import concurrencytest.data.Person;
-
 public class Main {
-
-    /**
-     * Setting this to 'true' will make the CommitLogListener fail with: Transaction must have 'STATUS_ACTIVE' to add a connection. Current status: STATUS_COMMITTED Everything works fine when using the build in Cayenne pool
-     */
-    private static final boolean useHikariCP = true;
 
     private static ServerRuntime _serverRuntime;
 
@@ -27,6 +21,10 @@ public class Main {
             ServerRuntimeBuilder srtBuilder = ServerRuntime.builder()
                     .addConfig( "cayenne-concurrencytest.xml" )
                     .addModule( CommitLogModule.extend().addListener( AfterUpdateListener.class ).module() );
+
+            // Setting this to 'true' will make the CommitLogListener fail with:
+            // Transaction must have 'STATUS_ACTIVE' to add a connection. Current status: STATUS_COMMITTED Everything works fine when using the build in Cayenne pool
+            boolean useHikariCP = true;
 
             if( useHikariCP ) {
                 HikariConfig config = new HikariConfig();
@@ -42,8 +40,9 @@ public class Main {
     }
 
     public static void main( String[] args ) {
-        Person person = serverRuntime().newContext().newObject( Person.class );
-        person.getObjectContext().commitChanges();
+        DataContext dataContext = (DataContext)serverRuntime().newContext();
+        dataContext.newObject( "Person" );
+        dataContext.commitChanges();
     }
 
     public static class AfterUpdateListener implements CommitLogListener {
@@ -51,9 +50,7 @@ public class Main {
         @Override
         public void onPostCommit( ObjectContext originatingContext, ChangeMap changes ) {
             new Thread( () -> {
-                ObjectSelect
-                        .query( Person.class )
-                        .select( serverRuntime().newContext() );
+                serverRuntime().newContext().select( new SelectQuery<>( "Person" ) );
             }, "afterUpdateThread" ).start();
         }
     }
--- src/main/java/concurrencytest/data/Person.java
+++
@@ -1,5 +0,0 @@
-package concurrencytest.data;
-
-import concurrencytest.data.auto._Person;
-
-public class Person extends _Person {}
